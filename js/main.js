da005f3c83e47220c01ea39640ee2bf2b9f4a172
Dilshodbek20040104 <dilshoditolmasov32@gmail.com>
2023-12-25 18:54:57 +0500

    tajriba uchun

---
+++ js/main.js
@@ -0,0 +1,473 @@
+// Homework
+
+// // ****** Begin-1  *****
+// let a;
+// let b;
+// let S;
+// let P;
+
+// // Tugri turtburchak tomonlari
+// a = 5;
+// b = 4;
+// // Tugri turtburchak yuzasi:
+// S = a * b;
+
+// console.log("Tugri turtburchak yuzasi:", S);
+// // Tugri turtbburchak perimetri
+// P = 2 * (a + b);
+// console.log("Tugri turtburchak perimetri:", P);
+
+// // ****** Begin-2  *****
+// // aylana uzunligi masalasi
+// let d;
+// let L;
+// const pi = 3.14;
+
+// // aylana diametri
+// d = 20;
+
+// // aylan uzunligi formulasi
+// L = pi * d;
+// // natija
+// console.log("aylana uzunligi :", L);
+
+// //****** Begin-3  *****
+// // Kub hajmini va tula sirtini topish masalasi
+
+// let a;
+// let V;
+// let S;
+// // kub tomoni
+// a = 20;
+// // Kub hajmi
+// V = a ** 3;
+// // Kubning tula sirti
+// S = 6 * a ** 2;
+// // Natijasi
+// console.log("kubning hajmi:", V);
+// console.log("kubning tula sirti:", S);
+
+// //****** Begin-4  *****
+// //  Paralleliped haqidagi masala
+// let a;
+// let b;
+// let c;
+// let S;
+// let V;
+
+// // Paralelepid tomonlari
+// a = 7;
+// b = 5;
+// c = 10;
+// // Paralelepid tula yuzasi:
+// S = 2 * (a * b + b * c + c * a);
+
+// // Paralelepid hajmi
+// V = a * b * c;
+
+// console.log("Paralleliped tula yuzasi:", S);
+
+// // Parallelepid hajmi
+// P = a * b * c;
+// console.log("Hajmi:", V);
+
+// //****** Begin-5  *****
+// // 2 sonning urta arifmetigini topish masalasi
+
+// let a;
+// let b;
+// let c;
+// let m;
+
+// // 2 sonlarni kiriting
+// a = 10;
+// b = 20;
+// // 2 sonning urta arifmetigi
+// m = (a + b) / 2;
+
+// // natijasi
+// console.log("2 sonning urta arifmetigi", m);
+
+// //****** Begin-6  *****
+// // Pifagor masalasi
+
+// let a;
+// let b;
+// let c;
+// let P;
+// // uchburchak katetlari
+// a = 3;
+// b = 4;
+
+// // gipotenuzasi topish formulasi
+// c = (a ** 2 + b ** 2) ** 0.5;
+
+// //  uchburchakning perimetri
+// P = a + b + c;
+
+// // natija
+// console.log("uchburchakning gipotenuzasi", c);
+// console.log("Uchburchakning perimetri", P);
+
+// //****** Begin-7  *****
+// // // 2ta ichma-ich aylana  orasidagi yuza topish masalasi r1>=r2
+
+// let r1;
+// let r2;
+// let S1;
+// let S2;
+// let S;
+
+// const p = 3.14;
+
+// // aylana raadiuslari
+// r1 = 5;
+// r2 = 3;
+
+// // 2aylana orasidagi yuza
+// S = p * r1 * r1 - p * r2 * r2;
+
+// // NATIJA
+// console.log("2 aylana orasidagi yuza ", S);
+
+// //****** Begin-8  *****
+// // aylana radiusi va yuzasini topish masalasi
+
+// // uzgaruvchilar
+// let l;
+// let r;
+// let s;
+// const p = 3.14;
+// // aylana uzunligi
+// l = 50 * p;
+// //  radiusini topish formulasi
+// r = l / (2 * p);
+// console.log("aylana radiusi", r);
+
+// // aylana yuzasi
+// s = p * r ** 2;
+// console.log("aylana yuzasi", s);
+
+// //****** Begin-9  *****
+// // 2nuqta orasidagi masofa
+
+// // uzgaruvchilar
+// let x1;
+// let x2;
+// let y1;
+// let y2;
+// let L;
+// x1 = 14;
+// x2 = 9;
+// y1 = 20;
+// y2 = 23;
+
+// // 2nuqta orasidagi masofa
+// L = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1 / 2);
+
+// console.log("2nuqta orasidagi masofa:", L);
+
+// //****** Begin-10  *****
+// // Sonlarni qiymatini  almashtirish masalasi
+
+// // uzgaruvchilar
+// let a;
+// let b;
+// let c;
+// let d;
+// let x;
+// let y;
+
+// a = 4;
+// b = 5;
+// c = 9;
+
+// // masalani ishlash jarayoni
+// d = a;
+// x = b;
+// y = c;
+
+// b = d;
+// c = x;
+// a = y;
+
+// // natija
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// // ****** Begin-11  *****
+// // Funksiyani qiymatini topish masalasi
+// // uzgaruvchilar
+// let x;
+// let y;
+
+// x = 13;
+
+// //masalani berilishi
+// y = 4 * (x - 3) ** 6 - 7 * (x - 3) ** 3 + 2;
+
+// // natijasi
+// console.log("y funksiyaning  qiymati", y);
+
+// // ****** Begin-12  *****
+// // A sonining darajalarini aniqlovchi dastur
+
+// // boshlangich qiymatlar
+// let a;
+// let y1;
+// let y2;
+// let y3;
+// let y4;
+
+// // a sonining qiymati
+// a = 10;
+
+// // topish kerak bulgan darajalari
+// y1 = a ** 3;
+// y2 = a ** 6;
+// y3 = a ** 9;
+// y4 = a ** 15;
+
+// // dastur natijasi
+// console.log("dasturning natijalari", y1);
+// console.log("dasturning natijalari", y2);
+// console.log("dasturning natijalari", y3);
+// console.log("dasturning natijalari", y4);
+
+// // ****** Begin-13  *****
+// // frengeyt temperaturadan selsiy temperaturasiga utish dasturi masalasi
+
+// //uzgaruvchilar
+// // T-frengeyt temperaturasi
+// // t-gradus selsiy  temperaturasi
+// let T;
+// let t;
+
+// // T ning qiymati ixtiyoriy tanlang
+// T = 200;
+
+// // selsiyga utish formulasi
+// t = ((T - 32) * 5) / 9;
+
+// // dastur natijasi
+// console.log("Gradus selsiydagi qiymati temperaturaning:", t);
+
+// // ****** Begin-14  *****
+// //  1kg shokolod va konfet narxlarinig farqlarini topish masalasi
+
+// let x;
+// let a;
+// let y;
+// let b;
+// let z;
+
+// // boshlangich qiymatlari
+// x = 4;
+// a = 200.0;
+// y = 6;
+// b = 145.0;
+// // farqini topish uchun
+// z = a / x - b / y;
+
+// // natijasi
+// console.log("2ta shirinlikning urtasidagi tafovut:", z, "sum");
+
+// // ****** Begin-15  *****
+// //  shokolod va konfet narxlarinig jami summasini topish masalasi
+
+// let x;
+// let a;
+// let y;
+// let b;
+// let z;
+
+// // boshlangich qiymatlari
+// x = 4;
+// a = 50.0;
+// y = 12;
+// b = 48.0;
+
+// // umumiy jami summani  topish uchun
+// z = a * x + b * y;
+
+// // natijasi
+// console.log("2ta shirinlikning jami summasi:", z, "ming sum");
+
+// *******  INTEGER toifasiga oid misollar *******
+
+// // Intereger-1
+// // 3xonali sonning strukturasi --> abc
+
+// // uzgaruvchilar
+// let xyz;
+// let z;
+// let y;
+
+// // 3xonali sonning boshlangich qiymati
+// xyz = 548;
+
+// // topish uchun formula
+// z = xyz % 10;
+// y = ((xyz - (xyz % 10)) / 10) % 10;
+// // dastur natijasi
+// console.log("3xonali sonning umumiy tuzilishi", xyz);
+// console.log("Birlar xonasidagi raqami", z);
+// console.log("Unlar xonasidagi raqami", y);
+
+// // Intereger-2
+// // 3xonali sonning strukturasi --> abc
+
+// // uzgaruvchilar
+// let xyz;
+// let z;
+// let y;
+// let x;
+// let d;
+
+// // 3xonali sonning boshlangich qiymati
+// xyz = 591;
+
+// // yigindisini topish uchun formula
+// z = xyz % 10;
+// y = ((xyz - (xyz % 10)) / 10) % 10;
+// x = (xyz - (xyz % 100)) / 100;
+// d = x + y + z;
+
+// // dastur natijasi
+// console.log("3xonali sonning umumiy tuzilishi", xyz);
+// console.log("Birlar xonasidagi raqami", z);
+// console.log("Unlar xonasidagi raqami", y);
+// console.log("Yuzlar xonasidagi raqami", x);
+// console.log("3xonali sonning raqamlar yigindisi", d);
+
+// // Intereger-3
+// // 3xonali sonning strukturasi --> abc
+
+// // uzgaruvchilar
+// let xyz;
+// let z;
+// let y;
+// let x;
+// let d;
+
+// // 3xonali sonning boshlangich qiymati
+// xyz = 123;
+
+// // Teskari sonni topish uchun formula
+// z = xyz % 10;
+// y = ((xyz - (xyz % 10)) / 10) % 10;
+// x = (xyz - (xyz % 100)) / 100;
+// d = z * 100 + y * 10 + x;
+
+// // dastur natijasi
+// console.log("3xonali sonning umumiy tuzilishi", xyz);
+// console.log("Birlar xonasidagi raqami", z);
+// console.log("Unlar xonasidagi raqami", y);
+// console.log("Yuzlar xonasidagi raqami", x);
+// console.log("3xonali sonning raqamlar yigindisi", d);
+
+// Integer-4
+// // 3xonali sonning boshlangich qiymati
+// xyz = 123;
+
+// //  Sonning urni almashturgandan sung hosil bulgan sonni  topish uchun formula
+// z = xyz % 10;
+// y = ((xyz - (xyz % 10)) / 10) % 10;
+// x = (xyz - (xyz % 100)) / 100;
+// d = y * 100 + x * 10 + z;
+
+// // dastur natijasi
+// console.log("3xonali sonning umumiy tuzilishi", xyz);
+// console.log("Birlar xonasidagi raqami", z);
+// console.log("Unlar xonasidagi raqami", y);
+// console.log("Yuzlar xonasidagi raqami", x);
+// console.log("3xonali sonning raqamlar yigindisi", d);
+
+// Integer-5
+// // 3xonali sonning boshlangich qiymati
+// xyz = 571;
+
+// //  Sonning urni almashturgandan sung hosil bulgan sonni  topish uchun formula
+// z = xyz % 10;
+// y = ((xyz - (xyz % 10)) / 10) % 10;
+// x = (xyz - (xyz % 100)) / 100;
+// d = x * 100 + z * 10 + y;
+
+// // dastur natijasi
+// console.log("3xonali sonning umumiy tuzilishi", xyz);
+// console.log("Birlar xonasidagi raqami", z);
+// console.log("Unlar xonasidagi raqami", y);
+// console.log("Yuzlar xonasidagi raqami", x);
+// console.log("3xonali sonning raqamlar yigindisi", d);
+
+// // Integer - 6;
+// // uzgaruvchilar
+// let a;
+// let k;
+// // 4xonali sonning boshlangich qiymati
+// a = 5718;
+
+// //  Sonning urni almashturgandan sung hosil bulgan sonni  topish uchun formula
+// k = ((a - (a % 100)) / 100) % 10;
+
+// // dastur natijasi
+// console.log("3xonali sonning umumiy tuzilishi", a);
+// console.log("Minglar xonasidagi raqami", k);
+
+// // Integer -7
+// // uzgaruvchilar
+
+// let N; // N-sekund(vaqt)
+// N = 4500;
+
+// let M; // M-minut(vaqt)
+
+// // formulasi minutga utish uchun
+// M = N / 60;
+
+// // dastur natijasi
+// console.log("Kun boshidan beri", M, "minut utdi");
+
+// // Integer -8
+// // uzgaruvchilar
+
+// let N; // N-sekund(vaqt)
+// N = 7200;
+
+// let H; // H-soat(vaqt)
+
+// // formulasi soatga utish uchun
+// H = N / 3600;
+
+// // dastur natijasi
+// console.log("Kun boshidan beri", H, "soat utdi");
+
+// // Integer -9
+// // uzgaruvchilar
+// let n;
+// let a;
+// let b;
+// let h;
+// let m;
+
+// n = 3748;
+// m = n / 60 - (n % 60) / 60;
+// s = (n % 3600) % 60;
+// console.log("kun boshidan utgan vaqt:", n, "sekond");
+// c = console.log("Kun boshidan beri", m, "minut", s, "sekond utdi.");
+
+// // Integer -10
+// // uzgaruvchilar
+// let n;
+// let a;
+// let b;
+// let h;
+// let m;
+
+// n = 3748;
+// h = n / 3600 - (n % 3600) / 3600;
+// m = (n % 3600) / 60 - ((n % 3600) % 60) / 60;
+// s = (n % 3600) % 60;
+// console.log("kun boshidan utgan vaqt:", n, "sekond");
+// c = console.log("Kun boshidan beri", h, "soat", m, "minut", s, "sekond utdi.");
